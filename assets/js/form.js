9ea61f5cba904b097396de7640c519f2d747a852
kristivent <ventk84@gmail.com>
2024-10-03 21:08:49 -0400

    added form javascript

---
+++ assets/js/form.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('form');
+
+// Listen for form submission event
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const values = {};
+
+    // Get values of inputs
+    // Values into an object
+    values.day = document.querySelector("[name = 'day']").value;
+    values.time = document.querySelector("[name = 'time']").value;
+    values.title = document.querySelector("[name = 'title']").value;
+    values.description = document.querySelector("[name = 'description']").value;
+    values.importance = document.querySelector("[name = 'importance']").value;
+    values.notes = document.querySelector("[name = 'notes']").value;
+
+    // get existing tasks from local storage
+    const existingTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    // add new task to existing array
+    existingTasks.push(values);
+
+    // Put updated array in local storage
+    localStorage.setItem('tasks', JSON.stringify(existingTasks));
+
+    // Redirect to planner page
+    location.assign('./index.html');
+
+})
+
+
+
+
+
+
